f50a75139a15a6325c4b1e6c1cb0f3c884b92ced
Andrew Moon <157635268+taehomoo@users.noreply.github.com>
2024-09-23 03:44:50 -0500

    Added MenuItem file

---
+++ MenuItem/MenuItem.java
@@ -0,0 +1,48 @@
+package menuitem;
+
+//class to create MenuItem objects
+
+public class MenuItem {
+    String name;
+    int calories;
+    int protein;
+    int carbs;
+    int fat;
+    int quantity = 1;
+
+    public MenuItem(String name, int calories, int protein, int carbs, int fat) {
+        this.name = name;
+        this.calories = calories;
+        this.protein = protein;
+        this.carbs = carbs;
+        this.fat = fat;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public int getProtein() {
+        return protein;
+    }
+
+    public int getCarbs() {
+        return carbs;
+    }
+
+    public int getFat() {
+        return fat;
+    }
+}
